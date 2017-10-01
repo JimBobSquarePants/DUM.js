a423211d720b9db9ccef52483da8b9c585ebd062
James Jackson-South <james_south@hotmail.com>
2017-10-01 18:04:14 +1100

    Begin add unit tests

---
+++ tests/unit/events.js
@@ -0,0 +1,52 @@
+QUnit.module("events", {
+    setup: function () {
+        document.body.focus();
+    },
+    teardown: globals.tearDown
+});
+
+QUnit.test("null or undefined handler", function (assert) {
+    assert.expect(8);
+
+    // on
+    try {
+        globals.addHandlers($DUM.on($DUM.id("firstp"), "click", null, null));
+        assert.ok(true, "Passing a null handler to on will not throw an exception");
+    } catch (e) { }
+
+    try {
+        globals.addHandlers($DUM.on($DUM.id("firstp"), "click", undefined, undefined));
+        assert.ok(true, "Passing an undefined handler to on will not throw an exception");
+    } catch (e) { }
+
+    try {
+        globals.addHandlers($DUM.on($DUM.id("firstp"), "click", null, e => { }));
+        assert.ok(true, "Passing a null selector to on will not throw an exception");
+    } catch (e) { }
+
+    try {
+        globals.addHandlers($DUM.on($DUM.id("firstp"), "click", undefined, e => { }));
+        assert.ok(true, "Passing an udefined selector to on will not throw an exception");
+    } catch (e) { }
+
+    // one
+    try {
+        $DUM.one($DUM.id("firstp"), "click", null, null);
+        assert.ok(true, "Passing a null handler to one will not throw an exception");
+    } catch (e) { }
+
+    try {
+        $DUM.one($DUM.id("firstp"), "click", undefined, undefined);
+        assert.ok(true, "Passing an undefined handler to one will not throw an exception");
+    } catch (e) { }
+
+    try {
+        $DUM.one($DUM.id("firstp"), "click", null, e => { });
+        assert.ok(true, "Passing a null selector to one will not throw an exception");
+    } catch (e) { }
+
+    try {
+        $DUM.one($DUM.id("firstp"), "click", undefined, e => { });
+        assert.ok(true, "Passing an udefined selector to one will not throw an exception");
+    } catch (e) { }
+});
---
+++ tests/unit/selectors.js
@@ -0,0 +1,33 @@
+QUnit.module("selectors", {
+    setup: function () {
+        document.body.focus();
+    },
+    teardown: globals.tearDown
+});
+
+QUnit.test("id selectors match", function (assert) {
+    assert.expect(1);
+
+    let actual = $DUM.id("firstp");
+    let expected = document.getElementById("firstp");
+
+    assert.equal(expected, actual, "id should return the same element as getElementById");
+});
+
+QUnit.test("query selectors match", function (assert) {
+    assert.expect(1);
+
+    let actual = $DUM.query("#firstp");
+    let expected = document.querySelector("#firstp");
+    assert.equal(expected, actual, "query should return the same element as querySelector");
+});
+
+QUnit.test("queryAll selectors match", function (assert) {
+    assert.expect(2);
+
+    let actual = $DUM.queryAll("#firstp");
+    let expected = Array.prototype.slice.call(document.querySelectorAll("#firstp"));
+
+    assert.equal(1, actual.length, "queryAll should have a value");
+    assert.deepEqual(expected, actual, "queryAll should return the same elements as querySelectorAll");
+});
