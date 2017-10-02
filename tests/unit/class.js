267816a54a3ed5a2b396953534d2f9bb69369b1c
James Jackson-South <james_south@hotmail.com>
2017-10-02 18:41:27 +1100

    Add toggleClass tests

--- tests/unit/class.js
+++ tests/unit/class.js
@@ -5,7 +5,7 @@ QUnit.module("classes", {
     teardown: globals.tearDown
 });
 
-QUnit.test("manipulate single classes", function (assert) {
+QUnit.test("add/remove single classes", function (assert) {
     assert.expect(2);
     let className = "customclass";
 
@@ -16,7 +16,7 @@ QUnit.test("manipulate single classes", function (assert) {
     assert.ok(!$DUM.id("firstp").classList.contains(className), "Can remove single classname");
 });
 
-QUnit.test("manipulate multiple classes", function (assert) {
+QUnit.test("add/remove multiple classes", function (assert) {
     assert.expect(4);
     let className = "customclass",
         className2 = "customclass2";
@@ -34,6 +34,39 @@ QUnit.test("manipulate multiple classes", function (assert) {
     assert.ok(expected, "Can add multiple classnames in array");
 
     $DUM.removeClass($DUM.id("firstp"), [className , className2]);
+    expected = !$DUM.id("firstp").classList.contains(className) && !$DUM.id("firstp").classList.contains(className2);
+    assert.ok(expected, "Can remove multiple classnames in array");
+});
+
+QUnit.test("toggle single classes", function (assert) {
+    assert.expect(2);
+    let className = "customclass";
+
+    $DUM.toggleClass($DUM.id("firstp"), className);
+    assert.ok($DUM.id("firstp").classList.contains(className), "Can add single classname");
+
+    $DUM.toggleClass($DUM.id("firstp"), className);
+    assert.ok(!$DUM.id("firstp").classList.contains(className), "Can remove single classname");
+});
+
+QUnit.test("toggle multiple classes", function (assert) {
+    assert.expect(4);
+    let className = "customclass",
+        className2 = "customclass2";
+
+    $DUM.toggleClass($DUM.id("firstp"), className + " " + className2);
+    let expected = $DUM.id("firstp").classList.contains(className) && $DUM.id("firstp").classList.contains(className2);
+    assert.ok(expected, "Can add multiple space-separated classnames");
+
+    $DUM.toggleClass($DUM.id("firstp"), className + " " + className2);
+    expected = !$DUM.id("firstp").classList.contains(className) && !$DUM.id("firstp").classList.contains(className2);
+    assert.ok(expected, "Can remove multiple space-separated classnames");
+
+    $DUM.toggleClass($DUM.id("firstp"), [className, className2]);
+    expected = $DUM.id("firstp").classList.contains(className) && $DUM.id("firstp").classList.contains(className2);
+    assert.ok(expected, "Can add multiple classnames in array");
+
+    $DUM.toggleClass($DUM.id("firstp"), [className , className2]);
     expected = !$DUM.id("firstp").classList.contains(className) && !$DUM.id("firstp").classList.contains(className2);
     assert.ok(expected, "Can remove multiple classnames in array");
 });
